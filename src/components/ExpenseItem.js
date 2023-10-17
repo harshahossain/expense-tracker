a89ac016072982026d0e8fd477ab7602e1b5abbc
Harsha Hossain <harshahossain@gmail.com>
2023-10-17 15:03:23 +0600

    Component Update
    Broken the code into more Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,20 @@
-export default function ExpenseItem() {
-  return <h2>Expense Item in here</h2>;
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+export default function ExpenseItem(props) {
+  const expenseDate = new Date(2023, 10, 5);
+  const expenseTitle = "Bike Installment";
+  const expenseAmount = "25000";
+  return (
+    <div className="expense-item">
+      {/* {console.log(props)} */}
+      {/* <div>{props.date}</div> */}
+      <ExpenseDate date={props.date} />
+      <h2>{props.title}</h2>
+      <div className="expense-item_description">
+        <div className="expense-item_price">{props.amount}$</div>
+      </div>
+    </div>
+  );
 }
