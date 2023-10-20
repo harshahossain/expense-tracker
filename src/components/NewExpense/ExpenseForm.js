d78193781cfbf3f0ff6312a8a9638493c34a13cd
Harsha Hossain <harshahossain@gmail.com>
2023-10-20 17:56:27 +0600

    Updated State Usage

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,36 +2,55 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 export default function ExpenseForm() {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    //differnce is here i have to update all 3 states always the upper would let us update one state at a time. cause this usestate takes object
-    entertedTitle: "",
-    entertedAmount: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   //differnce is here i have to update all 3 states always the upper would let us update one state at a time. cause this usestate takes object
+  //   entertedTitle: "",
+  //   entertedAmount: "",
+  //   enteredDate: "",
+  // });
+
+  //very important whenver we change state that depends on previous state we should do it in a function. AS in fucntion inside the setUserInput/
+  //Im not talking about that titlechangehandler fn is managing setuserinput but setUserInput will take prevState in its params and then the fucntion will execute where we change state's value
   const titleChangeHandler = (evt) => {
-    // setEnteredTitle(evt.target.value);
-    setUserInput({
-      //have to make sure other value doesnt get lost so exprade than change the neccessary?
-      ...userInput,
-      entertedTitle: evt.target.value,
-    });
+    setEnteredTitle(evt.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   entertedTitle: evt.target.value,
+    // });
+
+    // setUserInput((prevState) => {
+    //   //automatically takes old state regardless of name
+    //   //now here we should return the new state snapshot
+    //   return { ...prevState, entertedTitle: evt.target.value }; //why should I do it like this? cause react schedules state update so its necessary
+    //   //this just guarentees the state are correctly snapshotted and not backdated
+    // });
   };
   const amountChangeHandler = (evt) => {
-    setUserInput({
-      ...userInput,
-      entertedAmount: evt.target.value,
-    });
-    //setEnteredAmount(evt.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   entertedAmount: evt.target.value,
+    // });
+
+    setEnteredAmount(evt.target.value);
+    // setUserInput((oldState) => {
+    //   return { ...oldState, entertedAmount: evt.target.value };
+    // });
   };
   const dateChangeHandler = (evt) => {
-    setUserInput({
-      ...userInput,
-      entertedDate: evt.target.value,
-    });
-    //setEnteredDate(evt.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   entertedDate: evt.target.value,
+    // });
+
+    setEnteredDate(evt.target.value);
+
+    // setUserInput((oldState) => {
+    //   return { ...oldState, enteredDate: evt.target.value };
+    // });
   };
   return (
     <form>
