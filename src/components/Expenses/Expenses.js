acd8153cbbd3a099ba340c04eb8be0ecba58bcc8
Harsha Hossain <harshahossain@gmail.com>
2023-10-21 21:21:58 +0600

    Update Expenses.js
    Cleaned up unnecessary codes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,22 +8,6 @@ export default function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2023");
 
   const propingData = props.expenses.map((expense) => {
-    const month = expense.date.toLocaleString("en-US", { month: "long" });
-    const day = expense.date.toDateString("en-US", { day: "2-digit" });
-    // if (expense.date.getMonth() === 1) month = "January";
-    // if (expense.date.getMonth() === 2) month = "February";
-    // if (expense.date.getMonth() === 3) month = "March";
-    // if (expense.date.getMonth() === 4) month = "April";
-    // if (expense.date.getMonth() === 5) month = "May";
-    // if (expense.date.getMonth() === 6) month = "June";
-    // if (expense.date.getMonth() === 7) month = "July";
-    // if (expense.date.getMonth() === 8) month = "August";
-    // if (expense.date.getMonth() === 9) month = "September";
-    // if (expense.date.getMonth() === 10) month = "October";
-    // if (expense.date.getMonth() === 11) month = "November";
-    // if (expense.date.getMonth() === 12) month = "December";
-    //date={`${month} ${expense.date.getDate()}, ${day} ${expense.date.getFullYear()}`}   //its actually prop
-
     return (
       <ExpenseItem
         key={expense.id}
